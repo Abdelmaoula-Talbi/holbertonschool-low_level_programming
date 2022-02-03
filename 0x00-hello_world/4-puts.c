d820206675a298334705c2aa5cd74cb7a499846f
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-03 23:33:28 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
-/*
- * C program to using puts function to print a text
- * main : the entry point of the program
- */
+/* C program to using puts function to print a text*/
 #include <stdio.h>
+/* main : the entry point of the program*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
