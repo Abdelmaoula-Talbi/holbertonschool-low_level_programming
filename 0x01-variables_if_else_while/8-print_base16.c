20c3937f8b69fa5a26a0e863ec8f1204a2f0e05d
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-06 19:08:38 +0100

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n = '0';
+	char c = 'a';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
