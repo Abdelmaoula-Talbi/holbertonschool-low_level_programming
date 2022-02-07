f13e8a214ae2e19c1c33d702f643dbba857d7ae2
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-07 18:50:13 +0100

    4 digits comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,28 +6,28 @@
  */
 int main(void)
 {
-	int a;
-	int b;
+int a;
+int b;
 
-	for (a = 0; a <= 99; a++)
-	{
-		for (b = 0; b <= 99; b++)
-		{
-			if (((a / 10) < (b / 10)) || (((a / 10) == (b / 10)) && ((a % 10) < (b % 10))))
-			{
-				putchar('0' + a / 10);
-				putchar('0' + a % 10);
-				putchar(' ');
-				putchar('0' + b / 10);
-				putchar('0' + b % 10);
-				if (a != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (a = 0; a <= 99; a++)
+{
+for (b = 0; b <= 99; b++)
+{
+if (((a / 10) < (b / 10)) || (((a / 10) == (b / 10)) && ((a % 10) < (b % 10))))
+{
+putchar('0' + a / 10);
+putchar('0' + a % 10);
+putchar(' ');
+putchar('0' + b / 10);
+putchar('0' + b % 10);
+if (a != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
