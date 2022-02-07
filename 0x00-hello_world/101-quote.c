ed62babbb45816b5d6928dff4786a7e93cfd14d7
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-07 22:53:42 +0100

    print without printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,6 +9,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
 	return (1);
 }
