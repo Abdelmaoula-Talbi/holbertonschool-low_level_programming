dbb7a36aeff2e6271de07cf690d59272148d9a54
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-07 18:37:12 +0100

    4 digits comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = 0; b <= 99; b++)
+		{
+			if (((a / 10) < (b / 10)) || (((a /10) == (b/10)) && ((a % 10) < (b % 10))))
+			{	
+				putchar('0' + a / 10);
+				putchar('0' + a % 10);
+				putchar(' ');
+				putchar('0' + b / 10);
+				putchar('0' + b % 10);
+				if (a != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
