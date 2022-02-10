e2684552a24447951a1a25bba810e76a1ea9dcfa
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-10 18:06:34 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,6 +11,10 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
+		if (n == 100)
+		{
+			printf("Buzz\n");
+		}
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz ");
@@ -23,13 +27,10 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if (n == 100)
-		{
-			printf("%d", n);
-		}
 		else
+		{
 			printf("%d ", n);
+		}
 	}
-	putchar('\n');
 	return (0);
 }
