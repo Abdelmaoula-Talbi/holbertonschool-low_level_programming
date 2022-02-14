44e33b580f292d11519f8e952b15cbe5005a5973
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-14 23:56:48 +0100

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: a string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+	i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (j = (i - 1) / 2; str[j] != '\0'; j++)
+		{
+			_putchar(str[j + 1]);
+		}
+	}
+	_putchar('\n');
+}
