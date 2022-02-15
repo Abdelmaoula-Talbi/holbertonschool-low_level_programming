8cc8564979eeee0af40dd6fd931f2cbcdb9be674
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-15 15:28:24 +0100

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -25,7 +25,7 @@ void puts_half(char *str)
 	}
 	else if (i % 2 != 0)
 	{
-		for (j = (i / 2); str[j] <= str[i]; j++)
+		for (j = (i / 2) + 1; str[j] != '\0'; j++)
 		{
 			_putchar(str[j + 1]);
 		}
