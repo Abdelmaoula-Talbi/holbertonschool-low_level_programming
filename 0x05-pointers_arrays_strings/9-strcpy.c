93c67edfca256000325101ebad1e625d5c1896a8
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-15 23:25:22 +0100

    similar strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the strin, pointed to by src, to the buffer pointed to by dest.
+ * @dest: destination where the string will be copied
+ * @src: source from where the string will be copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char *p = dest;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (p);
+}
