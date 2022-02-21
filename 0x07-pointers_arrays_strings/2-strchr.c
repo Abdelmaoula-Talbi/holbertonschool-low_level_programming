898e5944e4709bd699b1613a0c7740fed05ce4a4
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-21 18:24:17 +0100

    similar strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,17 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p = &c;
 
-	for (i = 0; (s[i] == c); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (p);
+			return (s + i);
 		}
 	}
-	return (NULL);
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	return (0);
 }
