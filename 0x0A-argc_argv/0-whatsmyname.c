3d879cfad1be3d349c5b163b6769cfc098b4e5b9
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-02-25 12:28:35 +0100

    print the argv[0]

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,12 +7,8 @@
  * @argv: array of strings of arguments of the program.
  * Return: 0 (Success).
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int i;
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[0]);
-	}
-	return(0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
