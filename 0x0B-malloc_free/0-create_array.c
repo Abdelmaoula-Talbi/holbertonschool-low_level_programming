257f1adbb14e341687c31bb32fb5437390da6648
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-02 12:46:30 +0100

    create array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,16 +9,20 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
 
 	ptr = malloc(sizeof(c) * size);
-	ptr[0] = c;
 	if (size == 0)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
 	}
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	return (0);
+	return (ptr);
 }
