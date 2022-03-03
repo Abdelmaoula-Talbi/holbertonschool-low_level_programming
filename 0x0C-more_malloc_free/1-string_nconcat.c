7a47de3a38c48061903fd4ea623770a07199599a
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-03 15:57:31 +0100

    similar strncat function with use of malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat -  concatenates two strings.
+ * @s1: first string to concatenate.
+ * @s2: second string to concatenate.
+ * @n: number of bytes to concatenate from s2 to s1.
+ * Return: pointer to the allocated memory for concatenation.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, size;
+	char c;
+	char *p;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size = strlen(s1) + n;
+	p = malloc((sizeof(c) * size) + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= strlen(s1); i++)
+	{
+		p[i] = s1[i];
+	}
+	for (i = 0; i <= n; i++)
+	{
+		p[i + strlen(s1)] = s2[i];
+	}
+	return (p);
+}
