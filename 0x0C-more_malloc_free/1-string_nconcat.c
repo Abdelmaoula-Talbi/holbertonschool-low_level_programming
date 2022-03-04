24c5d1e46000a06be8e9085bcef56297337636f1
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-04 18:39:42 +0100

    similar strncat function with use of malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,11 +29,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= strlen(s1); i++)
+	for (i = 0; i < strlen(s1); i++)
 	{
 		p[i] = s1[i];
 	}
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		p[i + strlen(s1)] = s2[i];
 	}
