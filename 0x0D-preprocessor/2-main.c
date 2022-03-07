1f5670598b3c506fe2be6324a65b885a1ff880c3
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-07 19:38:49 +0100

    print a standard predefined macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - prints the name of the file that the program was compiled from.
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
