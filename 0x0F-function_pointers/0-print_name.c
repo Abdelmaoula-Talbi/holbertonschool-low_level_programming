b12f28a0acb09889d04e9c4ffd67681e1a9ca1f4
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-10 00:04:12 +0100

    print name with a function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * f - print array of characters.
- * @s: array to print.
- * Return: Nothing.
- */
-void f(char *s)
-{
-	int i;
-
-	for (i = 0; i != '\0'; i++)
-	{
-		_putchar (s[i]);
-	}
-}
-/**
  * print_name - prints a name.
  * @name: the name to print.
  * @f: function pointer to print the name.
