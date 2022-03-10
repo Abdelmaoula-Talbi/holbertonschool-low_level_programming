c7513b615c216d5c297e3c2df9f9ed45b9690836
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-10 02:42:28 +0100

    search for an integer in an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		cmp(array[i]);
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
-			break;
+			cmp(array[i]);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+				break;
+			}
 		}
 	}
 	return (-1);
