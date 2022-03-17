f4a10207310ab598976835dcfc7d87e71e2c27b6
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-17 19:51:57 +0100

    print linked list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	int nodes = 0;
+	size_t nodes = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-		printf("[%d] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		nodes++;
 		h = h->next;
 	}
