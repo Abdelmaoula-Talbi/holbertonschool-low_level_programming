0167eda7b0a73559ed50f564c6d0df6456eba29e
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-18 00:49:42 +0100

    freeing a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,11 +10,10 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (temp != NULL)
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
-		free(temp->str);
 		free(temp);
 	}
 }
