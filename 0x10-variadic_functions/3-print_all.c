15b88372892ca9c4377d401e3d04c21fc6816ed6
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-19 20:50:51 +0100

    function to print anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,101 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+typedef struct print_t
+{
+	char *c;
+	void (*func)(va_list);
+} pr_t;
+
+/**
+ * pr_char - print a char.
+ * @c: char to print.
+ * Return: Nothing.
+ */
+void pr_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * pr_int - print an integer.
+ * @i: integer to print.
+ * Return: Nothing.
+ */
+void pr_int(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * pr_float - print a float.
+ * @f: float to print.
+ * Return: Nothing.
+ */
+void pr_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * pr_string - print an array of characters.
+ * @str: string to print.
+ * Return: Nothing.
+ */
+void pr_string(va_list str)
+{
+	char *s;
+
+	s = va_arg(str, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", s);
+	}
+}
+
+/**
+ * print_all - prints anything of any type passed as
+ * variable argument to the variadic function.
+ * @format: list of types of arguments passed to the function.
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	pr_t prs[] = {
+		{"c", pr_char},
+		{"i", pr_int},
+		{"f", pr_float},
+		{"s", pr_string},
+		{NULL, NULL}
+	};
+	int i, j;
+	va_list ap;
+
+	va_start(ap, format);
+	i = 0;
+	while (format[i] != '\0' && format != NULL)
+	{
+		j = 0;
+		while (prs[j].c != NULL)
+		{
+			if(format[i] == *(prs[j].c))
+			{
+				prs[j].func(ap);
+				if (format[i + 1] != '\0')
+					printf(", ");
+				else
+					printf("\n");
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+}
