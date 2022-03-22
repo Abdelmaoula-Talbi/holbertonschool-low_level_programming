3ae5b5d4963fcbf533341011bb0d15cc0616a56c
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-22 16:44:39 +0100

    add new node in the front of linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,10 +5,12 @@
  * add_nodeint - add new node at the beginning of a listint_t list.
  * @head: pointer to pointer to the first node in the listint_t list.
  * @n: integer member of the listint_t list.
+ * Return: pointer to the new node added, NULL if failed.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 	{
 		return (NULL);
