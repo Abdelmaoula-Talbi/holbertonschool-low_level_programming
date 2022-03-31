15ac5bc68f03fe23e85e9af22de83868cd4ad740
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-31 18:55:24 +0100

    copy from file to another with system call

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments.
+ * @argv: argument vector.
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_r, ret_r, fd_w, ret_w;
+	char *buf_r;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s %s %s\n", argv[0], argv[1], argv[2]);
+		exit(97);
+	}
+	buf_r = malloc(1024);
+	if (buf_r == NULL)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	ret_r = read(fd_r, buf_r, 1024);
+	if (ret_r == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_w = creat(argv[2], 0664);
+	if (fd_w == -1)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
+	ret_w = write(fd_w, buf_r, ret_r);
+	if (ret_w == -1)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
+	free(buf_r);
+	close(fd_r);
+	if (fd_r == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_r);
+		exit(100);
+	}
+	close(fd_w);
+	if (fd_w == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
+	}
+	return (0);
+}
