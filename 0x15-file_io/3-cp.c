0d24895bb5063e3a60688514e17fa252d50971e5
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-04-01 16:38:01 +0100

    copy content of a file to another file with system call

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,51 +19,51 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		exit(97);
 		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	buf_r = malloc(1024);
 	if (buf_r == NULL)
 	{
-		exit(98);
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r == -1)
 	{
-		exit(98);
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	ret_r = read(fd_r, buf_r, 1024);
 	if (ret_r == -1)
 	{
-		exit(98);
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	fd_w = creat(argv[2], 0664);
 	if (fd_w == -1)
 	{
-		exit(99);
 		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
 	}
 	ret_w = write(fd_w, buf_r, ret_r);
 	if (ret_w == -1)
 	{
-		exit(99);
 		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
 	}
 	free(buf_r);
 	close(fd_r);
 	if (fd_r == -1)
 	{
-		exit(100);
 		dprintf(2, "Error: Can't close fd %d\n", fd_r);
+		exit(100);
 	}
 	close(fd_w);
 	if (fd_w == -1)
 	{
-		exit(100);
 		dprintf(2, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
 	}
 	return (0);
 }
