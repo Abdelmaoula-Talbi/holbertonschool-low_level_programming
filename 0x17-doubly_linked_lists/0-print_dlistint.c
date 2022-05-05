568d90c866f82925c814f8290e816ac8cbd8eb69
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-05-05 18:47:18 +0100

    fixing betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint: prints all elements of a dlistint_t list.
+ * print_dlistint - prints all elements of a dlistint_t list.
  * @h: dlistint_t list.
  * Return: number of nodes of the dlistint_t list h.
  */
@@ -9,8 +9,6 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
