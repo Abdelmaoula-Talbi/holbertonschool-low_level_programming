700b082f863c07b866837130c67add4e7cefc096
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-05-05 20:09:27 +0100

    add a new node at the front of doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,6 +4,7 @@
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
  * @head: doubly linked list to add a new node in.
  * @n: integer data stored in the new node.
+ * Return: the address of the new node, or NULL if it failed.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
@@ -18,6 +19,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if ((*head) != NULL)
 		(*head)->prev = new;
 	(*head) = new;
-	return(new);
+	return (new);
 }
 
