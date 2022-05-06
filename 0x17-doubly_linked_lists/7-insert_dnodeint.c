b92222ac27b7688872f2bbd0f46db8861e2f811b
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-05-06 15:15:33 +0100

    add a node at a given position in a doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *temp = *h, *temp2;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (new == NULL || h == NULL)
 		return (NULL);
 	new->n = n;
 	i = 0;
