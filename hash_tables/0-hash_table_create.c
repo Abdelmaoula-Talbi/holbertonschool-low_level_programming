0f34bf73666473bd7dac2ba77e7807b885f29b56
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-11 20:21:38 +0100

    fix code for calloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_node->value = NULL;
 	new_node->next = NULL;
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size - 1);
+	new_table->array = calloc(sizeof(hash_node_t), new_table->size);
 	i = 0;
 	while (i < size)
 	{
