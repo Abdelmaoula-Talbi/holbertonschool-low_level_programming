a010acad2ab3c8636942cb3393636b17eace6f70
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-12 19:03:40 +0100

    fix code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,7 +27,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (!ht->array[i])
 				new_node->next = NULL;
 			else
+			{
+				if (strcmp(key, ht->array[i]->key) == 0)
+					free(ht->array[i]->value);
 				new_node->next = ht->array[i];
+			}
 			ht->array[i] = new_node;
 		}
 		i++;
