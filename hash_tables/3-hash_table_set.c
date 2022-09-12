f72d1890611d7711ace83f15ebe6dddb2d35dd86
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-12 18:22:56 +0100

    fix code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -29,8 +29,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			else
 				new_node->next = ht->array[i];
 			ht->array[i] = new_node;
-			i++;
 		}
+		i++;
 	}
 	return (1);
 }
